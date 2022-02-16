197617ccb50f432f3539819a3cb68f7b7b14cfda
ManuelHMR <manuelhmrjf@gmail.com>
2022-02-16 14:04:06 -0300

    display todos os quizz

--- main.js
+++ main.js
@@ -2,8 +2,18 @@ const urlQuizz = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
 getQuizz()
 function getQuizz(){
   let quizzPromise = axios.get(urlQuizz)
-  quizzPromise.then(quizzPromiseRepesponse)
+  quizzPromise.then(displayQuizz)
 } 
-function quizzPromiseRepesponse(quizzRepesponse){
-  console.log(quizzRepesponse.data)
+function displayQuizz(quizzRepesponse){
+  let data = quizzRepesponse.data
+  console.log(data)
+  for(let i = 0; i < quizzRepesponse.data.length; i++){
+    let quizzDisplay = document.querySelector(".quizz-display")
+    quizzDisplay.innerHTML = quizzDisplay.innerHTML + `
+      <div class="quizz-box">
+        <img src="${data[i].image}"/>
+        <h5>${data[i].title}</h5>
+      </div>
+    `
+  }
 }
