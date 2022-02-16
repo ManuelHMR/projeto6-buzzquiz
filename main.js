1699ab60b24e996cbbedad24b7efa9fd6e86faef
ManuelHMR <manuelhmrjf@gmail.com>
2022-02-16 17:02:09 -0300

    remove a tela 1 ao clicar em um quizz

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-const urlQuizz = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+const urlQuizz = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes'
 getQuizz()
-function getQuizz(){
+function getQuizz() {
   let quizzPromise = axios.get(urlQuizz)
   quizzPromise.then(displayQuizz)
-} 
-function displayQuizz(quizzRepesponse){
+}
+function displayQuizz(quizzRepesponse) {
   let data = quizzRepesponse.data
   console.log(data)
-  for(let i = 0; i < quizzRepesponse.data.length; i++){
-    let quizzDisplay = document.querySelector(".quizz-display")
-    quizzDisplay.innerHTML = quizzDisplay.innerHTML + `
-      <div class="quizz-box">
+  for (let i = 0; i < quizzRepesponse.data.length; i++) {
+    let quizzDisplay = document.querySelector('.quizz-display')
+    quizzDisplay.innerHTML =
+      quizzDisplay.innerHTML +
+      `
+      <div class="quizz-box" onclick="displayScreen2(this)">
         <img src="${data[i].image}"/>
         <h5>${data[i].title}</h5>
       </div>
     `
   }
+}
+function displayScreen2() {
+  hideScreen1()
+}
+function hideScreen1() {
+  document.querySelector('header').classList.add('hidden')
+  document.querySelector('main').classList.add('hidden')
 }
